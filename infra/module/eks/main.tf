db42d912e41030fd7e06e1e43f705c4693e2690b
kennethchinedu <anamsken60@gmail.com>
2025-07-02 19:45:16 +0100

    setting up eks

--- infra/module/eks/main.tf
+++ infra/module/eks/main.tf
@@ -15,30 +15,83 @@
 
 # }
 
+#using EKS module from terraform-aws-modules
+# This module creates an EKS cluster with a single node group.
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.37"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 20.37"
 
-  cluster_name    = "ek8s-cluster"
-  cluster_version = "1.31"
+#   cluster_name    = "ek8s-cluster"
+#   cluster_version = "1.31"
 
-  # Optional
-  cluster_endpoint_public_access = true
+#   # Optional
+#   cluster_endpoint_public_access = true
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
+#   # Optional: Adds the current caller identity as an administrator via cluster access entry
+#   enable_cluster_creator_admin_permissions = true
 
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
+#   cluster_compute_config = {
+#     enabled    = true
+#     node_pools = ["general-purpose"]
+#   }
+
+#   vpc_id     = var.vpc_id
+#   subnet_ids = ["var.private_subnet_ids"]
+
+#   tags = merge(
+#   local.common_tags,
+#   { Name = "k8s" } )
+# }
+
+
+
+resource "aws_eks_cluster" "ek8s" {
+  name = "example"
+
+  access_config {
+    authentication_mode = "API"
   }
 
-  vpc_id     = var.vpc_id
-  subnet_ids = ["var.private_subnet_ids"]
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.32"
 
-  tags = merge(
-  local.common_tags,
-  { Name = "k8s" } )
+  vpc_config {
+    subnet_ids = [
+      var.private_subnet_ids,
+      var.public_subnet_ids,
+      
+    ]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
 }
 
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
--- infra/module/eks/variable.tf
+++ infra/module/eks/variable.tf
@@ -17,13 +17,13 @@ locals {
 
 
 #Defining LOCALS for VPC and subnets
-locals {
-  vpc_id = var.vpc_id
-  private_subnet_ids = var.public_subnet_ids
-  public_subnet_cidr = var.private_subnet_ids
-#   availability_zone_a = var.availability_zone_a
-#   availability_zone_b = var.availability_zone_b
-}  
+# locals {
+#   vpc_id = locals.vpc_id
+# #   private_subnet_ids = var.public_subnet_ids
+# #   public_subnet_ids = var.private_subnet_ids
+# #   availability_zone_a = var.availability_zone_a
+# #   availability_zone_b = var.availability_zone_b
+# }  
 
 
 
